c4132f32871dbcc62539001eedffa1481df8bd3d
mathias labora martirena <mathiaslabora@gmail.com>
2021-10-01 09:26:36 -0300

    0.1

--- src/script/script.js
+++ src/script/script.js
@@ -1,24 +1,51 @@
-let productos = [];
-let venta = [];
+let products = [];
+let sale = [];
 
 document.addEventListener("DOMContentLoaded", function (e) {
     let descr = document.getElementById('desc')
     let name = document.getElementById('nomb')
-    let costo = document.getElementById('costo')
-    let cant = document.getElementById('stock')
+    let cost = document.getElementById('costo')
+    let amount = document.getElementById('stock')
 
-    document.getElementById('ing').addEventListener('click', ()=>{
-        if(true) {
-            let obj ={}
-            obj.descripcion=(descr.value);
+    let listProdTrans = document.getElementById('elSelect');
+    let option = document.createElement('option');
+
+    document.getElementById('ing').addEventListener('click', () => {
+        if (descr.value && name.value && amount.value && cost.value) {
+            let obj = {}
+            obj.descripcion = descr.value;
             obj.nombre = name.value;
-            obj.costo = costo.value;
-            obj.cantidad = cant.value;
-            productos.push(obj);
-        }
+            obj.costo = cost.value;
+            obj.cantidad = amount.value;
+            products.push(obj);
+            option.text = 
+        } /* else {
 
+        } */
+        console.log(products)
     })
-    console.log(productos)
+
+
+    let typeTrans;
+    document.getElementById('trans').addEventListener('change', () => {
+        typeTrans = document.getElementById('trans').value
+    });
+    let dateTrans;
+    let subTotal;
+    let iva;
+
+document.getElementById('add').addEventListener('click',()=>{
+
+    let obj = {}
+    obj.descripcion = (descr.value);
+    obj.nombre = name.value;
+    obj.costo = cost.value;
+    obj.cantidad = amount.value;
+    sale.push(obj);
+
+})
+    
+
 
 
 });
